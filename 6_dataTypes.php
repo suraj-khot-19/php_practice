2cacf3e672070f12a624168837163ca9f194b66a
suraj-khot-19 <142373628+suraj-khot-19@users.noreply.github.com>
2024-03-14 18:19:28 +0530

    added datatype.php

---
+++ 6_dataTypes.php
@@ -0,0 +1,14 @@
+<?php
+    $a="suraj"; //string
+    $b=20;  //integer
+    $c=20.55;   //float
+    $d=true;    //boolean
+    $e=array(1,2,3);    //array
+    $f=null;    //null
+    //$g=new class(); //object
+
+    //var_dump($varname); used to check data type.
+    var_dump($a);   //string(5) "suraj"
+    var_dump($e);   //array(3) { [0]=> int(1) [1]=> int(2) [2]=> int(3) }
+    var_dump($f);   //NULL
+?>
