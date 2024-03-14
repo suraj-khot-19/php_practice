39ca42640d860729d7e6f5aa4add9d030fcbb92b
suraj-khot-19 <142373628+suraj-khot-19@users.noreply.github.com>
2024-03-14 18:02:35 +0530

    added variable.php

---
+++ 5_variable.php
@@ -0,0 +1,38 @@
+<?php
+    //$ symbol is used for creating or using a variable
+
+    $a="suraj";
+    $marks=81.72;
+    echo $a;
+
+    echo "hello ". $a . " sir";
+
+    echo "<h1>". $a ."</h1>";
+
+    echo "how are you " . $a;
+
+    echo "i got ".$marks;
+
+    //these two are consider as diffrent
+    $age=20;
+    $AGE=20;
+    echo $age;
+    echo $AGE;
+
+    //accepted varibles by php
+    $_name="suraj";
+    $my_name="suraj";
+    $myName="suraj";
+    $myname99="suraj";
+    $name="suraj";
+
+    //non variables
+    /*errors
+
+    $20name="suraj";
+    $my name="suraj";
+    $my%name="suraj";
+    $my-name="suraj";
+
+    */
+?>
